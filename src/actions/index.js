5e3968aa5c99c58fc65c5bcb5adabdadc7b43805
SHIRA BEN ZAKEN <shira441192@gmail.com>
2021-11-17 23:14:40 +0200

    use lodash for memorize
    lesson 280

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import JsonPlaceHolder from "../apis/JsonPlaceHolder";
 
 export const fetchPost = () => async (dispatch) => {
@@ -6,8 +7,13 @@ export const fetchPost = () => async (dispatch) => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize( async (id, dispatch) => {
     const response = await JsonPlaceHolder.get( `/users/${id}` );
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-}
+});
+
